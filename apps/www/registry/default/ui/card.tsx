960025a94f818c3afffaf2fbdd430397793101f0
pnodet <paul.nodet@gmail.com>
2023-11-15 01:35:37 +0700

    fix(card): add className props validation

--- apps/www/registry/default/ui/card.tsx
+++ apps/www/registry/default/ui/card.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 const Card = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+  React.HTMLAttributes<HTMLDivElement> & {readonly className?: string}
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
@@ -19,7 +19,7 @@ Card.displayName = "Card"
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+  React.HTMLAttributes<HTMLDivElement> & {readonly className?: string}
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
@@ -31,7 +31,7 @@ CardHeader.displayName = "CardHeader"
 
 const CardTitle = React.forwardRef<
   HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
+  React.HTMLAttributes<HTMLHeadingElement> & {readonly className?: string}
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
@@ -46,7 +46,7 @@ CardTitle.displayName = "CardTitle"
 
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
+  React.HTMLAttributes<HTMLParagraphElement> & {readonly className?: string}
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
@@ -58,7 +58,7 @@ CardDescription.displayName = "CardDescription"
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+  React.HTMLAttributes<HTMLDivElement> & {readonly className?: string}
 >(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ))
@@ -66,7 +66,7 @@ CardContent.displayName = "CardContent"
 
 const CardFooter = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+  React.HTMLAttributes<HTMLDivElement> & {readonly className?: string}
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
